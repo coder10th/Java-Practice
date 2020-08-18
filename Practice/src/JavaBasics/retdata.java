15f2f2651562c1b9a3346df2019039e9e181be09
Auth@Test <Auth@Test>
2020-08-18 16:21:58 -0500

    added a line

--- Practice/src/JavaBasics/retdata.java
+++ Practice/src/JavaBasics/retdata.java
@@ -4,8 +4,8 @@ public class retdata {
 
 	public static void main(String[] args) {
 		Userdata user=new Userdata();
-		user.setFirstName("Rekha");
-		user.setLastName("Gavini");
+		user.setFirstName("Tom");
+		user.setLastName("Jerry");
 		user.output();
 	}
 
